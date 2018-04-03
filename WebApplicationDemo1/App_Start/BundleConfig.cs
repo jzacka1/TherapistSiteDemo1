abc00c04f0e5054de4501543440c28f2c97e0780
jzacka1 <jameszacka@gmail.com>
2018-04-03 00:55:27 -0400

    Changes in style.

--- WebApplicationDemo1/App_Start/BundleConfig.cs
+++ WebApplicationDemo1/App_Start/BundleConfig.cs
@@ -23,10 +23,6 @@ namespace WebApplicationDemo1
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
-
             bundles.Add(new StyleBundle("~/Content/PsychologistCSS").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/SitePsychologist.css"));
